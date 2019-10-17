799a262b63f5e0c30f3338f36f2d069a969f9800
Your Name <you@example.com>
2019-10-17 15:50:12 +0700

    Add Diem.java

---
+++ Documents/NetBeansProjects/DesginPatten-FACTORY PATTERN/src/SinhVien.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SV
+ */
+public class SinhVien {
+ 
+    private String hoTen;
+    private int diem1,diem2;
+    public SinhVien(String hoTen,int diem1,int diem2){
+        this.hoTen=hoTen;
+        this.diem1=diem1;
+        this.diem2=diem2;
+    }
+    public void tinhDTB(){
+        System.out.println();
+    }
+} 
+
